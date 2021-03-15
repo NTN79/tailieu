7deeb2773cc5cc6880e0d99bb49c87e2865b2ea7
kymap057 <vohoangky147@gmail.com>
2021-03-15 16:52:07 +0700

    add user

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use((error,req,res,next)=>{
 });
 
 //app listen port
-const port = 8080 ||process.env.PORT ;
+const port = process.env.PORT|| 8080 ;
 app.listen(port,()=>{
     console.log(`app listen port: " ${port} "`);
 })  
